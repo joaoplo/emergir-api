68114e9af07c179ff05d6c52e069a3075fd36a31
João Pedro Lima <joaopedro.oliveira@poupex.com.br>
2025-02-27 12:56:39 -0300

    feat: adicionando classes de EvolucaoClinicaEntity e FichaAnamneseEntity

--- src/main/java/com/java/emergir_api/domain/FichaAnamneseEntity.java
+++ src/main/java/com/java/emergir_api/domain/FichaAnamneseEntity.java
@@ -1,13 +1,32 @@
-/*
 package com.java.emergir_api.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.java.emergir_api.domain.entities.PacienteEntity;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.time.LocalDate;
 
 @Data
 @Entity
-@Table( name = "FICHA_ANAMNESE", schema = "EMERGIR_DIGITAL" )
+@Table( name = "FICHAS_ANAMNESE", schema = "EMERGIR_DIGITAL" )
 public class FichaAnamneseEntity{
+
+    @Id
+    @Column( name = "IDT_FICHAS_ANAMNESE" )
+    @SequenceGenerator( name = "SEQ_FICHAS_ANAMNESE", sequenceName = "SEQ_FICHAS_ANAMNESE", allocationSize = 1 , schema = "CMC_DIGITAL" )
+    @GeneratedValue( strategy = GenerationType.SEQUENCE, generator = "SEQ_FICHAS_ANAMNESE" )
+    private Long id;
+
+    @Column( name = "DESCRICAO", nullable = false )
+    private String descricao;
+
+    @Column( name = "DT_REGISTRO", nullable = false )
+    private LocalDate dtRegistro;
+
+    @Column( name = "IDT_PACIENTE", nullable = false, insertable = false, updatable = false )
+    private Long idPaciente;
+
+    @OneToOne
+    @JoinColumn( name = "IDT_PACIENTE", unique = true )
+    private PacienteEntity paciente;
 }
-*/
