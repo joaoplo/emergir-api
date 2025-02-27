84fdb0e2df2fab59250f7b7ede117aaf660ce4cf
João Pedro Lima <joaopedro.oliveira@poupex.com.br>
2025-02-27 15:35:56 -0300

    feat: adicionando classe repositories

---
+++ src/main/java/com/java/emergir_api/repositories/AvaliacaoPsicologicaRepository.java
@@ -0,0 +1,7 @@
+package com.java.emergir_api.repositories;
+
+import com.java.emergir_api.domain.entities.AvaliacaoPsicologicaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AvaliacaoPsicologicaRepository extends JpaRepository<AvaliacaoPsicologicaEntity, Long>{
+}
---
+++ src/main/java/com/java/emergir_api/repositories/FichaAnamneseRepository.java
@@ -0,0 +1,7 @@
+package com.java.emergir_api.repositories;
+
+import com.java.emergir_api.domain.entities.FichaAnamneseEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FichaAnamneseRepository extends JpaRepository<FichaAnamneseEntity, Long> {
+}
